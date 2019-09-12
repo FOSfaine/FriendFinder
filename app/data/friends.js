f7b96cb057254c55206aea1ed83e1aeeb580a804
Esther Cho <Eunsem.64642@gmail.com>
2019-09-12 16:21:08 -0600

    initial html files

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friendsArray = [{
+
+            name: "Kate",
+            photo: "https://......jpg",
+            scores: [
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+            ]
+        },
+        {
+            name: "Irina",
+            photo: "https://......jpg",
+            scores: [
+                3,
+                5,
+                4,
+                2,
+                1,
+                5,
+                4,
+                2,
+                5,
+                2
+            ]
+        },
+        {
+            name: "Jillian",
+            photo: "https://......jpg",
+            scores: [
+                2,
+                3,
+                4,
+                1,
+                1,
+                5,
+                5,
+                2,
+                4,
+                3
+            ]
+        },
+        {
+            name: "Ameth",
+            photo: "https://......jpg",
+            scores: [
+                3,
+                3,
+                4,
+                4,
+                2,
+                4,
+                2,
+                2,
+                5,
+                5
+            ]
+        },
+        {
+            name: "Joaquim",
+            photo: "https://......jpg",
+            scores: [
+                5,
+                3,
+                2,
+                2,
+                1,
+                3,
+                5,
+                2,
+                4,
+                2
+            ]
+        }
+
+
+        module.exports = friendsArray;
